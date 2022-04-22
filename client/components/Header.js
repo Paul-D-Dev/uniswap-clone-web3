76cbee2aa9ad743ac21f6be2d7bf53dc5cd5b54c
Paul DAO <dao.van.paul@gmail.com>
2022-04-21 23:51:49 -0400

    WIP HEADER: UI

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Image from 'next/image';
+import ethLogo from '../assets/eth.png'
+import uniSwapLogo from '../assets/uniswap.png'
+import {FiArrowUpRight} from "react-icons/all";
+
+const style = {
+    wrapper: '',
+    headerLogo: '',
+    nav: '',
+    navItemContainer: '',
+    navItem: '',
+    activeNavItem: ''
+
+};
 
 const Header = () => {
-    return <div>Header</div>
+
+    const [selectedNav, setSelectedNav] = useState('swap');
+
+    return (
+        <div className={style.wrapper}>
+            <div className={style.headerLogo}>
+                <Image src={uniSwapLogo} height={40} width={40} alt='uniswap'/>
+            </div>
+            <nav className={style.nav}>
+                <div className={style.navItemContainer}>
+                    <div
+                        onClick={() => setSelectedNav('swap')}
+                        className={`${style.navItem} ${selectedNav === 'swap' && style.activeNavItem}`}>
+                        Swap
+                    </div>
+                    <div
+                        onClick={() => setSelectedNav('pool')}
+                        className={`${style.navItem} ${selectedNav === 'pool' && style.activeNavItem}`}>
+                        Pool
+                    </div>
+                    <div
+                        onClick={() => setSelectedNav('vote')}
+                        className={`${style.navItem} ${selectedNav === 'vote' && style.activeNavItem}`}>
+                        Vote
+                    </div>
+                    <a
+                        href="https://info.uniswap.org/#"
+                        target='_blank'
+                        rel='noreferrer'>
+                        <div
+                            onClick={() => setSelectedNav('chart')}
+                            className={style.navItem}>
+                            Chart <FiArrowUpRight/>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Header;
