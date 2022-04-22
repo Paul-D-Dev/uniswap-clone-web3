dd7ed14d6fcd0a78c44f0dfe21c1e6a45011262d
Paul DAO <dao.van.paul@gmail.com>
2022-04-21 23:34:15 -0400

    Feat Header component: create header

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
+import Header from '../components/Header'
 
 const style = {
   wrapper: 'h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between',
@@ -7,7 +7,7 @@ const style = {
 const Home: NextPage = () => {
   return (
     <div className={style.wrapper}>
-
+    <Header />
     </div>
   )
 }
