cf772ea0e6c5b25a463c8ed2a19ed007b849caf5
Paul DAO <dao.van.paul@gmail.com>
2022-04-22 12:33:54 -0400

    WIP HEADER: set styles + fix import package

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,17 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import Image from 'next/image';
-import ethLogo from '../assets/eth.png'
-import uniSwapLogo from '../assets/uniswap.png'
-import {FiArrowUpRight} from "react-icons/all";
+import ethLogo from '../assets/eth.png';
+import uniSwapLogo from '../assets/uniswap.png';
+import {FiArrowUpRight} from "react-icons/fi";
 
 const style = {
-    wrapper: '',
-    headerLogo: '',
-    nav: '',
-    navItemContainer: '',
-    navItem: '',
-    activeNavItem: ''
-
+    wrapper: `p-4 w-screen flex justify-between items-center`,
+    headerLogo: `flex w-1/4 items-center justify-start`,
+    nav: `flex-1 flex justify-center items-center`,
+    navItemsContainer: `flex bg-[#191B1F] rounded-3xl`,
+    navItem: `px-4 py-2 m-1 flex items-center text-lg font-semibold text-[0.9rem] cursor-pointer rounded-3xl`,
+    activeNavItem: `bg-[#20242A]`,
+    buttonsContainer: `flex w-1/4 justify-end items-center`,
+    button: `flex items-center bg-[#191B1F] rounded-2xl mx-2 text-[0.9rem] font-semibold cursor-pointer`,
+    buttonPadding: `p-2`,
+    buttonTextContainer: `h-8 flex items-center`,
+    buttonIconContainer: `flex items-center justify-center w-8 h-8`,
+    buttonAccent: `bg-[#172A42] border border-[#163256] hover:border-[#234169] h-full rounded-2xl flex items-center justify-center text-[#4F90EA]`,
 };
 
 const Header = () => {
@@ -24,7 +29,7 @@ const Header = () => {
                 <Image src={uniSwapLogo} height={40} width={40} alt='uniswap'/>
             </div>
             <nav className={style.nav}>
-                <div className={style.navItemContainer}>
+                <div className={style.navItemsContainer}>
                     <div
                         onClick={() => setSelectedNav('swap')}
                         className={`${style.navItem} ${selectedNav === 'swap' && style.activeNavItem}`}>
@@ -47,7 +52,8 @@ const Header = () => {
                         <div
                             onClick={() => setSelectedNav('chart')}
                             className={style.navItem}>
-                            Chart <FiArrowUpRight/>
+                            Chart
+                            <FiArrowUpRight/>
                         </div>
                     </a>
                 </div>
